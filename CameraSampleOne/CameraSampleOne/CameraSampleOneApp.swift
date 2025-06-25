abada5952695a85400747e51ab91c5f033828128
angel zambrano <angeldzambrano99@gmail.com>
2025-06-25 15:51:07 -0700

    added camera app

---
+++ CameraSampleOne/CameraSampleOne/CameraSampleOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraSampleOneApp.swift
+//  CameraSampleOne
+//
+//  Created by angel zambrano on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct CameraSampleOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
