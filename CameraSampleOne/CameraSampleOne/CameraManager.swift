cb23bba4595e76d7c5f78d2036c64a61494c4a90
angel zambrano <angeldzambrano99@gmail.com>
2025-06-25 18:15:53 -0700

    camera works

--- CameraSampleOne/CameraSampleOne/CameraManager.swift
+++ CameraSampleOne/CameraSampleOne/CameraManager.swift
@@ -6,231 +6,234 @@
 //
 
 import Foundation
+import SwiftUI
 
-class CameraManager: NSObject, ObservableObject {
-    @Published var isSessionRunning = false
-    @Published var isPaused = false
-    @Published var showAlert = false
-    @Published var alertMessage = ""
-    
-    let session = AVCaptureSession()
-    private var videoDeviceInput: AVCaptureDeviceInput?
-    private var photoOutput: AVCapturePhotoOutput?
-    
-    override init() {
-        super.init()
-        setupCamera()
-        setupSessionObservers()
-    }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self)
-    }
-    
-    private func setupSessionObservers() {
-          if #available(iOS 18.0, *) {
-              NotificationCenter.default.addObserver(
-                  self,
-                  selector: #selector(sessionDidStartRunning),
-                  name: AVCaptureSession.didStartRunningNotification,
-                  object: session
-              )
-              
-              NotificationCenter.default.addObserver(
-                  self,
-                  selector: #selector(sessionDidStopRunning),
-                  name: AVCaptureSession.didStopRunningNotification,
-                  object: session
-              )
-          } else {
-              NotificationCenter.default.addObserver(
-                  self,
-                  selector: #selector(sessionDidStartRunning),
-                  name: .AVCaptureSessionDidStartRunning,
-                  object: session
-              )
-              
-              NotificationCenter.default.addObserver(
-                  self,
-                  selector: #selector(sessionDidStopRunning),
-                  name: .AVCaptureSessionDidStopRunning,
-                  object: session
-              )
-          }
-      }
-    
-    @objc private func sessionDidStartRunning() {
-        DispatchQueue.main.async {
-            self.isSessionRunning = true
-        }
-    }
-    
-    @objc private func sessionDidStopRunning() {
-        DispatchQueue.main.async {
-            self.isSessionRunning = false
-        }
-    }
-    
-    func requestPermissions() {
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .authorized:
-            DispatchQueue.global().async {
-                self.startSession()
-            }
-        case .notDetermined:
-            AVCaptureDevice.requestAccess(for: .video) { granted in
-                if granted {
-                    DispatchQueue.global().async {
-                        self.startSession()
-                    }
-                }
-            }
-        case .denied, .restricted:
-            showAlert(message: "Camera access is required to use this app")
-        @unknown default:
-            break
-        }
-        
-        // Also request photo library permissions
-        PHPhotoLibrary.requestAuthorization { status in
-            if status != .authorized {
-                DispatchQueue.main.async {
-                    self.showAlert(message: "Photo library access is required to save photos")
-                }
-            }
-        }
-    }
-    
-    func togglePause() {
-        if isPaused {
-            resumeCamera()
-        } else {
-            pauseCamera()
-        }
-    }
-    
-    private func pauseCamera() {
-        guard isSessionRunning && !isPaused else { return }
-        
-        DispatchQueue.global(qos: .userInitiated).async {
-            self.session.beginConfiguration()
-            
-            if let videoInput = self.videoDeviceInput {
-                self.session.removeInput(videoInput)
-            }
-            
-            self.session.commitConfiguration()
-            
-            DispatchQueue.main.async {
-                self.isPaused = true
-            }
-        }
-    }
-    
-    private func resumeCamera() {
-        guard isPaused else { return }
-        
-        DispatchQueue.global(qos: .userInitiated).async {
-            self.session.beginConfiguration()
-            
-            if let videoInput = self.videoDeviceInput {
-                if self.session.canAddInput(videoInput) {
-                    self.session.addInput(videoInput)
-                }
-            }
-            
-            self.session.commitConfiguration()
-            
-            DispatchQueue.main.async {
-                self.isPaused = false
-            }
-        }
-    }
-    
-    private func setupCamera() {
-        session.sessionPreset = .high
-        
-        // Add video input
-        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
-            showAlert(message: "Could not find camera")
-            return
-        }
-        
-        do {
-            videoDeviceInput = try AVCaptureDeviceInput(device: videoDevice)
-            
-            if session.canAddInput(videoDeviceInput!) {
-                session.addInput(videoDeviceInput!)
-            }
-            
-            // Configure for 60fps
-            try configureFor60FPS(device: videoDevice)
-            
-        } catch {
-            showAlert(message: "Could not create video device input: \(error.localizedDescription)")
-            return
-        }
-        
-        // Add photo output
-        photoOutput = AVCapturePhotoOutput()
-        if session.canAddOutput(photoOutput!) {
-            session.addOutput(photoOutput!)
-        }
-    }
-    
-    private func configureFor60FPS(device: AVCaptureDevice) throws {
-        try device.lockForConfiguration()
-        
-        // Find the best format that supports 60fps
-        let formats = device.formats
-        var bestFormat: AVCaptureDevice.Format?
-        var bestFrameRateRange: AVFrameRateRange?
-        
-        for format in formats {
-            for range in format.videoSupportedFrameRateRanges {
-                if range.maxFrameRate >= 60.0 {
-                    if bestFormat == nil ||
-                       CMVideoFormatDescriptionGetDimensions(format.formatDescription).width >
-                       CMVideoFormatDescriptionGetDimensions(bestFormat!.formatDescription).width {
-                        bestFormat = format
-                        bestFrameRateRange = range
-                    }
-                }
-            }
-        }
-        
-        if let format = bestFormat, let _ = bestFrameRateRange {
-            device.activeFormat = format
-            device.activeVideoMinFrameDuration = CMTimeMake(value: 1, timescale: 60)
-            device.activeVideoMaxFrameDuration = CMTimeMake(value: 1, timescale: 60)
-        }
-        
-        device.unlockForConfiguration()
-    }
-    
-    private func startSession() {
-        guard !session.isRunning else { return }
-        
-        session.startRunning()
-        
-        DispatchQueue.main.async {
-            self.isPaused = false
-        }
-    }
-    
-    func capturePhoto() {
-        guard let photoOutput = photoOutput, !isPaused else { return }
-        
-        let settings = AVCapturePhotoSettings()
-        settings.flashMode = .auto
-        
-        photoOutput.capturePhoto(with: settings, delegate: self)
-    }
-    
-    private func showAlert(message: String) {
-        DispatchQueue.main.async {
-            self.alertMessage = message
-            self.showAlert = true
-        }
-    }
-}
+import Photos
+
+//class CameraManager: NSObject, ObservableObject {
+//    @Published var isSessionRunning = false
+//    @Published var isPaused = false
+//    @Published var showAlert = false
+//    @Published var alertMessage = ""
+//    
+//    let session = AVCaptureSession()
+//    private var videoDeviceInput: AVCaptureDeviceInput?
+//    private var photoOutput: AVCapturePhotoOutput?
+//    
+//    override init() {
+//        super.init()
+//        setupCamera()
+//        setupSessionObservers()
+//    }
+//    
+//    deinit {
+//        NotificationCenter.default.removeObserver(self)
+//    }
+//    
+//    private func setupSessionObservers() {
+//          if #available(iOS 18.0, *) {
+//              NotificationCenter.default.addObserver(
+//                  self,
+//                  selector: #selector(sessionDidStartRunning),
+//                  name: AVCaptureSession.didStartRunningNotification,
+//                  object: session
+//              )
+//              
+//              NotificationCenter.default.addObserver(
+//                  self,
+//                  selector: #selector(sessionDidStopRunning),
+//                  name: AVCaptureSession.didStopRunningNotification,
+//                  object: session
+//              )
+//          } else {
+//              NotificationCenter.default.addObserver(
+//                  self,
+//                  selector: #selector(sessionDidStartRunning),
+//                  name: .AVCaptureSessionDidStartRunning,
+//                  object: session
+//              )
+//              
+//              NotificationCenter.default.addObserver(
+//                  self,
+//                  selector: #selector(sessionDidStopRunning),
+//                  name: .AVCaptureSessionDidStopRunning,
+//                  object: session
+//              )
+//          }
+//      }
+//    
+//    @objc private func sessionDidStartRunning() {
+//        DispatchQueue.main.async {
+//            self.isSessionRunning = true
+//        }
+//    }
+//    
+//    @objc private func sessionDidStopRunning() {
+//        DispatchQueue.main.async {
+//            self.isSessionRunning = false
+//        }
+//    }
+//    
+//    func requestPermissions() {
+//        switch AVCaptureDevice.authorizationStatus(for: .video) {
+//        case .authorized:
+//            DispatchQueue.global().async {
+//                self.startSession()
+//            }
+//        case .notDetermined:
+//            AVCaptureDevice.requestAccess(for: .video) { granted in
+//                if granted {
+//                    DispatchQueue.global().async {
+//                        self.startSession()
+//                    }
+//                }
+//            }
+//        case .denied, .restricted:
+//            showAlert(message: "Camera access is required to use this app")
+//        @unknown default:
+//            break
+//        }
+//        
+//        // Also request photo library permissions
+//        PHPhotoLibrary.requestAuthorization { status in
+//            if status != .authorized {
+//                DispatchQueue.main.async {
+//                    self.showAlert(message: "Photo library access is required to save photos")
+//                }
+//            }
+//        }
+//    }
+//    
+//    func togglePause() {
+//        if isPaused {
+//            resumeCamera()
+//        } else {
+//            pauseCamera()
+//        }
+//    }
+//    
+//    private func pauseCamera() {
+//        guard isSessionRunning && !isPaused else { return }
+//        
+//        DispatchQueue.global(qos: .userInitiated).async {
+//            self.session.beginConfiguration()
+//            
+//            if let videoInput = self.videoDeviceInput {
+//                self.session.removeInput(videoInput)
+//            }
+//            
+//            self.session.commitConfiguration()
+//            
+//            DispatchQueue.main.async {
+//                self.isPaused = true
+//            }
+//        }
+//    }
+//    
+//    private func resumeCamera() {
+//        guard isPaused else { return }
+//        
+//        DispatchQueue.global(qos: .userInitiated).async {
+//            self.session.beginConfiguration()
+//            
+//            if let videoInput = self.videoDeviceInput {
+//                if self.session.canAddInput(videoInput) {
+//                    self.session.addInput(videoInput)
+//                }
+//            }
+//            
+//            self.session.commitConfiguration()
+//            
+//            DispatchQueue.main.async {
+//                self.isPaused = false
+//            }
+//        }
+//    }
+//    
+//    private func setupCamera() {
+//        session.sessionPreset = .high
+//        
+//        // Add video input
+//        guard let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+//            showAlert(message: "Could not find camera")
+//            return
+//        }
+//        
+//        do {
+//            videoDeviceInput = try AVCaptureDeviceInput(device: videoDevice)
+//            
+//            if session.canAddInput(videoDeviceInput!) {
+//                session.addInput(videoDeviceInput!)
+//            }
+//            
+//            // Configure for 60fps
+//            try configureFor60FPS(device: videoDevice)
+//            
+//        } catch {
+//            showAlert(message: "Could not create video device input: \(error.localizedDescription)")
+//            return
+//        }
+//        
+//        // Add photo output
+//        photoOutput = AVCapturePhotoOutput()
+//        if session.canAddOutput(photoOutput!) {
+//            session.addOutput(photoOutput!)
+//        }
+//    }
+//    
+//    private func configureFor60FPS(device: AVCaptureDevice) throws {
+//        try device.lockForConfiguration()
+//        
+//        // Find the best format that supports 60fps
+//        let formats = device.formats
+//        var bestFormat: AVCaptureDevice.Format?
+//        var bestFrameRateRange: AVFrameRateRange?
+//        
+//        for format in formats {
+//            for range in format.videoSupportedFrameRateRanges {
+//                if range.maxFrameRate >= 60.0 {
+//                    if bestFormat == nil ||
+//                       CMVideoFormatDescriptionGetDimensions(format.formatDescription).width >
+//                       CMVideoFormatDescriptionGetDimensions(bestFormat!.formatDescription).width {
+//                        bestFormat = format
+//                        bestFrameRateRange = range
+//                    }
+//                }
+//            }
+//        }
+//        
+//        if let format = bestFormat, let _ = bestFrameRateRange {
+//            device.activeFormat = format
+//            device.activeVideoMinFrameDuration = CMTimeMake(value: 1, timescale: 60)
+//            device.activeVideoMaxFrameDuration = CMTimeMake(value: 1, timescale: 60)
+//        }
+//        
+//        device.unlockForConfiguration()
+//    }
+//    
+//    private func startSession() {
+//        guard !session.isRunning else { return }
+//        
+//        session.startRunning()
+//        
+//        DispatchQueue.main.async {
+//            self.isPaused = false
+//        }
+//    }
+//    
+//    func capturePhoto() {
+//        guard let photoOutput = photoOutput, !isPaused else { return }
+//        
+//        let settings = AVCapturePhotoSettings()
+//        settings.flashMode = .auto
+//        
+//        photoOutput.capturePhoto(with: settings, delegate: self)
+//    }
+//    
+//    private func showAlert(message: String) {
+//        DispatchQueue.main.async {
+//            self.alertMessage = message
+//            self.showAlert = true
+//        }
+//    }
+//}
