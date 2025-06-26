ff7d01fb2a3ec8b46c3563eeec7385f61f8f81ac
angel zambrano <angeldzambrano99@gmail.com>
2025-06-25 21:41:52 -0700

    added attributes

--- CameraSampleOne/CameraSampleOne/Camera/CameraAttributes.swift
+++ CameraSampleOne/CameraSampleOne/Camera/CameraAttributes.swift
@@ -6,14 +6,54 @@
 //
 
 import Foundation
+import AVKit
+
 
 struct CameraManagerAttributes {
     var capturedMedia: CameraMedia? = nil
     var error: CameraError? = nil
+    var outputType: CameraOutputType = .photo
+    var cameraPosition: CameraPosition = .back
+    var zoomFactor: CGFloat = 1.0
+    var flashMode: CameraFlashMode = .off
+    var resolution: AVCaptureSession.Preset = .hd1920x1080
 }
 
 
 public enum CameraError: Error {
     case cameraPermissionsNotGranted
     case cannotSetupInput, cannotSetupOutput
+}
+
+
+// MARK: Camera Output Type
+public enum CameraOutputType: CaseIterable {
+    case photo
+    case video
+}
+
+// MARK: Camera Position
+public enum CameraPosition: CaseIterable {
+    case back
+    case front
+}
+
+// MARK: Camera Flash Mode
+public enum CameraFlashMode: CaseIterable {
+    case off
+    case on
+    case auto
+}
+
+// MARK: Camera Light Mode
+public enum CameraLightMode: CaseIterable {
+    case off
+    case on
+}
+
+// MARK: Camera HDR Mode
+public enum CameraHDRMode: CaseIterable {
+    case off
+    case on
+    case auto
 }
