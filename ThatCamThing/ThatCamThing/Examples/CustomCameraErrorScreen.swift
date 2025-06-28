a08d6e349f43cd357b2de3f24a752021b765009f
angel zambrano <angeldzambrano99@gmail.com>
2025-06-27 17:44:50 -0700

    cleaning up

--- ThatCamThing/ThatCamThing/Examples/CustomCameraErrorScreen.swift
+++ ThatCamThing/ThatCamThing/Examples/CustomCameraErrorScreen.swift
@@ -1,55 +1,10 @@
 //
-//  DefaultErrorScreen.swift
+//  CustomCameraErrorScreen.swift
 //  ThatCamThing
 //
 //  Created by angel zambrano on 6/27/25.
 //
-
 import SwiftUI
-
-struct DefaultErrorScreen: ErrorScreenView {
-    let error: Error
-    let onRetry: () -> Void
-    
-    init(error: Error, onRetry: @escaping () -> Void) {
-        self.error = error
-        self.onRetry = onRetry
-    }
-    
-    var body: some View {
-        VStack(spacing: 20) {
-            Image(systemName: "exclamationmark.triangle.fill")
-                .font(.system(size: 50))
-                .foregroundColor(.red)
-            
-            Text("Camera Error!!!!!!!!")
-                .font(.title2)
-                .fontWeight(.bold)
-            
-            Text(error.localizedDescription)
-                .font(.body)
-                .multilineTextAlignment(.center)
-                .foregroundColor(.secondary)
-            
-            Button("Try Again") {
-                onRetry()
-            }
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.blue)
-            .cornerRadius(8)
-        }
-        .padding()
-        .background(Color(.systemBackground))
-        .cornerRadius(12)
-        .shadow(radius: 10)
-        .padding()
-    }
-}
-
-#Preview {
-//    DefaultErrorScreen(error: ., onRetry: <#() -> Void#>)
-}
 
 struct CustomCameraErrorScreen: ErrorScreenView {
     let error: Error
